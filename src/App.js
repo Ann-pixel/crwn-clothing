d71fbf949377970d6c01a366218e58083ac41833
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2020-11-13 08:48:44 -0500

    adding header component and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage.components/homepage.component";
-import {Route} from "react-router-dom";
-import shopPage from "./pages/shop/shop.components";
-
+import {Route, Switch} from "react-router-dom";
+import ShopPage from "./pages/shop/shop.components";
+import "./components/header/header.component";
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-   
+    <Header /> 
+    <Switch>
     <Route exact path= '/' component= {HomePage} />
-    <Route exact path = '/shop' component= {shopPage}   />
-
+    <Route exact path = '/shop' component= {ShopPage}   />
+    </Switch>
     
     </div>
   );
--- src/components/collection-preview/collection-preview.components.jsx
+++ src/components/collection-preview/collection-preview.components.jsx
@@ -6,7 +6,9 @@ const CollectionPreview = ({title, items}) =>(
     <div className ="collection-preview">
         <h1 className="title"> {title.toUpperCase()} </h1>
         <div className ="preview">
-            {items.filter((item, idx) => idx<4).map(({id, ...otherItemPreview}) =>(
+            {items
+            .filter((item, idx) => idx<4)
+            .map(({id, ...otherItemPreview}) =>(
             <CollectionItem key={id} {...otherItemPreview} />    
             ))}
         </div>
