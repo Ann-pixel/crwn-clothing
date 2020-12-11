8fe4fdce337c2add7b1bed6b6fb2bad18bcff9f8
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2020-12-11 09:20:27 -0500

    moved sign in and signup styles to a styled component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const SignInAndSignUp = styled.div`
+    width: 850px;
+    display: flex;
+    justify-content: space-between;
+    margin: 30px auto;
+`;
+export default SignInAndSignUp;
