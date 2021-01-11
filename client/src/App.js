7c04aad0d1d1f153ceae6d522a81bdf203d053ac
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-01-11 11:22:37 -0500

    added lazy loading and suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { connect } from "react-redux";
 import { GlobalStyle } from "./global.styles";
-import HomePage from "./pages/homepage.components/homepage.component";
 import { Route, Switch, Redirect } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.components";
 import "./components/header/header.component";
 import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { checkUserSession } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
-import CheckoutPage from "./pages/check-out/check-out.component";
+
+import Spinner from "./components/spinner/spinner.component";
+
+const HomePage = lazy(() =>
+  import("./pages/homepage.components/homepage.component")
+);
+
+const ShopPage = lazy(() => import("./pages/shop/shop.components"));
+
+const CheckoutPage = lazy(() =>
+  import("./pages/check-out/check-out.component")
+);
+const SignInAndSignUpPage = lazy(() =>
+  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+);
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -22,16 +33,19 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-          }
-        />
-        <Route exact path="/checkout" component={CheckoutPage} />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={HomePage} />
+
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+          <Route exact path="/checkout" component={CheckoutPage} />
+        </Suspense>
       </Switch>
     </div>
   );
--- client/src/pages/shop/shop.components.jsx
+++ client/src/pages/shop/shop.components.jsx
@@ -1,39 +1,40 @@
-import React, {useEffect} from "react";
-import {Route} from "react-router-dom";
-import CollectionsContainer from "../collections/collections.container";
-import {connect} from "react-redux";
-import {fetchCollectionsStart} from "../../redux/shop/shop.actions";
+import React, { lazy, useEffect, Suspense } from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from "../../components/spinner/spinner.component";
+const CollectionsContainer = lazy(() =>
+  import("../collections/collections.container")
+);
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collections-overview.container")
+);
 
-
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
-
-
-const ShopPage = ({fetchCollectionsStart, match})=> {
-  useEffect(()=>{
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  },[fetchCollectionsStart]);
-  
-    return (
-        <div className= "shop-page">   
-        <Route exact 
-          path=  { `${match.path}`} 
-          component= {CollectionsOverviewContainer} />
-        <Route exact 
-          path= { `${match.path}/:collectionId`}  
-          component= {CollectionsContainer} />
-        </div>
-    )
-  }  
+  }, [fetchCollectionsStart]);
 
+  return (
+    <Suspense fallback={<Spinner />}>
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          component={CollectionsContainer}
+        />
+      </div>
+    </Suspense>
+  );
+};
 
-
-
-
-const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStart: ()=> dispatch(fetchCollectionsStart())
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
-    
-   
-
