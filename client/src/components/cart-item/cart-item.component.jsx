5b9f7adbf81632adb385144dd6591e40b091ad78
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-01-13 11:17:01 -0500

    lesson in performance. added memo to cart dropdown

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 import CartItemStyles from "./cart-item.styles";
 
-const CartItem = ({item: {imageUrl, price, name, quantity}}) =>
-(
-    <CartItemStyles>
-        <img src ={imageUrl} alt= "item"/>
-        <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price"> {quantity} x ${price}</span>
-        </div>
-
-
-        </CartItemStyles>
-
-)
-export  default CartItem;
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <CartItemStyles>
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {" "}
+        {quantity} x ${price}
+      </span>
+    </div>
+  </CartItemStyles>
+);
+export default React.memo(CartItem);
